0b916c05e1045ee7f273d89fa19b81f3e2998598
Adrian Nocek <ejdi177@gmail.com>
2020-05-10 00:17:06 +0100

    czesc zadania domowego

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const firstName = 'Adrian';
-const age = '30';
+const firstName = "Adrian";
+const age = "30";
 
-console.log(firstName)
-console.log(age)
-console.log(`Mam na imie ${firstName} i mam ${age} lat.`)
+console.log(firstName);
+console.log(age);
+console.log(`Mam na imie ${firstName} i mam ${age} lat.`);
